9c7ab5dc36d2e1f32298f8c61ee244f0a15b4c2b
bruno-caxa <frian6123@gmail.com>
2025-10-01 17:59:34 -0300

    initial commit

---
+++ src/main/java/com/barbershop/controller/Schedule.java
@@ -0,0 +1,15 @@
+package com.barbershop.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/schedule")
+public class Schedule {
+
+    @GetMapping("/")
+    public String getSchedule() {
+        return "This is the schedule endpoint.";
+    }
+}
